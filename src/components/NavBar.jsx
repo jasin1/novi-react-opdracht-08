93cb82de5d37f3b6933d9c89f7e0ccf30577f508
jasin1 <jasin.tairaidrissi@gmail.com>
2024-01-19 18:25:43 +0100

    added files and code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,64 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from '../assets/banana-01.png';
-import { useNavigate, Link } from 'react-router-dom';
+import {useNavigate, Link} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function NavBar() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <nav>
-        <Link to="/">
+    const {myAuth, logoutFunction} = useContext(AuthContext);
+    // console.log(toggleAuth);
+    console.log('new user', myAuth.user.myUser);
+
+
+
+    // console.log(whatsInTheContext);
+
+    return (
+        <nav>
+            <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
             <h3>
               Banana Security
             </h3>
           </span>
-        </Link>
+            </Link>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => navigate('/signin')}
-        >
-          Log in
-        </button>
-        <button
-          type="button"
-          onClick={() => navigate('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
-    </nav>
-  );
+            <div>
+
+                {!myAuth.isAuth ? (
+                    <div>
+                        <button
+                            type="button"
+                            onClick={() => navigate('/signin')}
+                        >
+                            Log in
+                        </button>
+
+                        <button
+                            type="button"
+                            onClick={() => navigate('/signup')}
+                        >
+                            Registreren
+                        </button>
+                    </div>
+                ) : (
+                    <div>
+
+                        <p>{myAuth.user.myUser}</p>
+                        <button
+                            type="button"
+                            onClick={logoutFunction}
+                        >Log out
+                        </button>
+
+                    </div>
+                )}
+
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
